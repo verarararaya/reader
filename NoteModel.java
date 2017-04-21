add54d2e384c6e1f905474926d8a7c0943b0cf4d
verarararaya <qwkq__39@126.com>
2017-04-21 22:44:59 +0800

    Add files via upload

---
+++ NoteModel.java
@@ -0,0 +1,86 @@
+package com.app.model;
+
+import java.util.Date;
+
+import org.xutils.db.annotation.Column;
+import org.xutils.db.annotation.Table;
+
+@Table(name = "configmodel",
+onCreated = "") 
+public class NoteModel {
+   @Column(
+           name = "ID",
+           isId = true,
+           autoGen = true
+   )
+	   private long id;
+   
+	   @Column(name = "TITLE",property = "NOT NULL")//
+	   private String title;
+	   	
+	   @Column(name = "CONTENT",property = "NOT NULL")//
+	   private String content;
+	
+	   @Column(name = "DATE",property = "NOT NULL")//
+	   private Date date;
+	   
+	   
+	   
+
+	public NoteModel() {
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	@Override
+	public String toString() {
+		return "NoteModel [id=" + id + ", title=" + title + ", content="
+				+ content + ", date=" + date + "]";
+	}
+
+	public NoteModel(String title, String content, Date date) {
+		super();
+		this.title = title;
+		this.content = content;
+		this.date = date;
+	}
+	   
+	   
+	   
+	   
+	   
+   
+   
+   
+   
+}
